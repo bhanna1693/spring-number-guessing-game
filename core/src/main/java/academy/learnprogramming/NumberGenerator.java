4739eca32a3c71ba6613cd65f26b234eb1c32b89
bhanna1693 <bhanna1693@gmail.com>
2019-02-24 15:16:31 -0500

    initial commit

---
+++ core/src/main/java/academy/learnprogramming/NumberGenerator.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public interface NumberGenerator {
+
+    int next();
+
+    int getMaxNumber();
+
+    int getMinNumber();
+}
---
+++ core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -0,0 +1,40 @@
+package academy.learnprogramming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class NumberGeneratorImpl implements NumberGenerator {
+
+    // fields
+    private final Random random = new Random();
+
+    private final int maxNumber;
+
+    private final int minNumber;
+
+    // == contructors ==
+    @Autowired
+    public NumberGeneratorImpl(@MaxNumber int maxNumber, @MinNumber int minNumber) {
+        this.maxNumber = maxNumber;
+        this.minNumber = minNumber;
+    }
+
+    // == public methods ==
+    @Override
+    public int next() {
+        return random.nextInt(maxNumber - minNumber) + minNumber;
+    }
+
+    @Override
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    @Override
+    public int getMinNumber() {
+        return minNumber;
+    }
+}
